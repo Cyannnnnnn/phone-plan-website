7e9adefb013e02f26953c5f4271796141d19e67e
react <cyanpan23@gmail.com>
2026-08-12 22:03:22 -0700

    Adding more code to the scollable Ad for go back to school section

--- app/components/BacktoSchool.tsx
+++ app/components/BacktoSchool.tsx
@@ -1,7 +1,56 @@
+"use client"
+
 import styles from "@/app/styles/backtoSchool.module.css"
 import Image from "next/image"
+import { supabase } from "../lib/superbase"
+import { useEffect, useState } from "react";
+import type { ScrollAds } from "../types/type";
 
 export default function BacktoSchool() {
+
+    const [data, setData] = useState<ScrollAds[]>([]);
+    const [activeNum, setActiveNum] = useState(0);
+
+    useEffect(() => {
+
+        async function fetchData() {
+            const {data, error} = await supabase
+                                        .from("scrollPhoneAd")
+                                        .select();
+            
+            if(error) {
+                console.error("Error when fetching data", error);
+                return
+            }
+
+            setData(data);
+            
+        }
+
+        fetchData();
+    }, [])
+
+    console.log("The fetched data is: ", data)
+
+    const allCards = data.map((card) => {
+        return (
+            <div key={card.id} className={styles.card}>
+                <p>{card.firstTitle}</p>
+                <p>{card.bigTitle}</p>
+                <p>{card.medTitle}</p>
+                <p>{card.smallTitle}</p>
+                <Image 
+                    className={styles.phonePic}
+                    src={card.src} 
+                    width={500} 
+                    height={500}
+                    alt="One of the phone Ad"
+                />
+            </div>
+        )
+    })
+    
+
     return (
         <section className={styles.mainContainer}>
             <Image 
@@ -9,6 +58,16 @@ export default function BacktoSchool() {
                 src="/backtoSchoolAd.webp" fill 
                 alt="Back to school Ad Pic" 
             />
+            <div className={styles.outsideContainer}>
+
+                <div className={`${styles.cardContainer}`} style={{left: `calc(-${activeNum * 100}% - ${activeNum}rem)`}}>
+                    {allCards}
+                </div>
+
+            </div>
+
+            <button onClick={() => setActiveNum(prev => prev - 1)}>{`<`}</button>
+            <button onClick={() => setActiveNum(prev => prev + 1)}>{`>`}</button>
         </section>
     )
 }
